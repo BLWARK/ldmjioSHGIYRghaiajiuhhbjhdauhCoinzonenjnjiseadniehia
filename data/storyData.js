43db287d4e678d18876c8ef58965beebb06d634d
Ronald <rozu0980@gmail.com>
2024-12-23 11:18:41 +0700

    test terus

--- data/storyData.js
+++ data/storyData.js
@@ -2,7 +2,7 @@ const storyData = [
   [
       {
         id: 1,
-        thumbnail: "/Story1 tumb.jpg",
+        thumbnail: "/Story222 tumb.jpg",
         content: [
           
           { type: "video", src: "/Story2.mp4", duration: 5000 },
@@ -13,7 +13,7 @@ const storyData = [
     ],
     [
       {
-        id: 3,
+        id: 2,
         thumbnail: "/Story1 tumb.jpg",
         content: [
           { type: "image", src: "/Story1 tumb.jpg", duration: 5000 },
@@ -38,7 +38,7 @@ const storyData = [
 
     [
       {
-        id: 4,
+        id: 3,
         thumbnail: "/Story3 tumb.jpg",
         content: [
           { type: "image", src: "/Story3 tumb.jpg", duration: 5000 },
@@ -50,7 +50,7 @@ const storyData = [
 
     [
       {
-        id: 5,
+        id: 4,
         thumbnail: "/Story4 tumb.jpg",
         content: [
           { type: "image", src: "/Story4 tumb.jpg", duration: 5000 },
@@ -62,7 +62,7 @@ const storyData = [
 
     [
       {
-        id: 6,
+        id: 5,
         thumbnail: "/Story5 tumb.jpg",
         content: [
           { type: "image", src: "/Story5 tumb.jpg", duration: 5000 },
