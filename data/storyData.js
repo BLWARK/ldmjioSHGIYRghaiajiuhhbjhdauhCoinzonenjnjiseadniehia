05e1bc66a820bae5637556fcfc6464979f76381b
Ronald <rozu0980@gmail.com>
2024-12-23 11:21:06 +0700

    changes thumb

--- data/storyData.js
+++ data/storyData.js
@@ -2,7 +2,7 @@ const storyData = [
   [
       {
         id: 1,
-        thumbnail: "/Story222 tumb.jpg",
+        thumbnail: "/Story2222 tumb.jpg",
         content: [
           
           { type: "video", src: "/Story2.mp4", duration: 5000 },
